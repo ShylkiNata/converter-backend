06dd406b3189f3320249ea3a1d77e9ccbffc3ef7
Natalya Shylkina <ns@albedo-tech.com>
2019-02-21 15:53:35 +0200

    add compressor

--- src/app/Http/Controllers/Api/FileController.php
+++ src/app/Http/Controllers/Api/FileController.php
@@ -2,16 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Helpers\ActionHelper;
-use App\Helpers\Converter;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\File;
-use Imagick;
-use Intervention\Image\Facades\Image;
-use Mockery\Exception;
-use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Process\Process;
 
@@ -20,10 +13,38 @@ class FileController extends Controller
     public function compress(Request $request, $type)
     {
         foreach ($request->all() as $key=>$value) {
-            /*$file = $request->file($key);
-            $img = Image::make($file);
-            var_dump($img->filesize());
-            var_dump($img->encode($type, 85)->encoded);*/
+            $uid = uniqid();
+            $destination = storage_path("app/public/images/$uid");
+
+            File::makeDirectory($destination);
+
+            try {
+                foreach ($request->files as $name => $file) {
+                    $from = $file->getRealPath();
+                    $to = "$destination/$name.$type";
+
+                    if($type === 'pdf') {
+                        $cmd = "gs -sDEVICE=pdfwrite -sOutputFile=$to $from";
+                    }
+                    else {
+                        $cmd = "convert $from -quality 50 $to";
+                    }
+                    $this->runProcess($cmd);
+                }
+
+                $pathToDownload = $this->archive($uid);
+
+                return response()->json([
+                    'ok' => $pathToDownload,
+                    'errors' => false
+                ], 200);
+            }
+            catch (\Exception $e) {
+                return response()->json([
+                    'ok' => false,
+                    'errors' => $e->getMessage()
+                ], 500);
+            }
         }
     }
     public function convert(Request $request, $type)
@@ -31,7 +52,7 @@ class FileController extends Controller
         $types = explode('-to-', $type);
 
         $uid = uniqid();
-        $destination = storage_path("images/$uid");
+        $destination = storage_path("app/public/images/$uid");
 
         File::makeDirectory($destination);
 
@@ -42,8 +63,11 @@ class FileController extends Controller
 
                 $this->runProcess("convert $from $to");
             }
+
+            $pathToDownload = $this->archive($uid);
+
             return response()->json([
-                'ok' => $uid,
+                'ok' => $pathToDownload,
                 'errors' => false
             ], 200);
         }
@@ -54,8 +78,9 @@ class FileController extends Controller
             ], 500);
         }
     }
+
     public function archive($uid) {
-        $folder = storage_path("images/$uid/");
+        $folder = storage_path("app/public/images/$uid/");
 
         if(!File::exists($folder)) {
             return response()->json([
@@ -65,12 +90,16 @@ class FileController extends Controller
         }
 
         try {
+            $publicZip = "storage/images/$uid/$uid.zip";
             $zipPath = "$folder$uid.zip";
             $this->runProcess("cd $folder; zip -c $zipPath *");
 
+            /*
             return response()
-                ->download($zipPath)
+                ->download($public)
                 ->deleteFileAfterSend(false);
+            */
+            return $publicZip;
         }
         catch (\Exception $e) {
             return response()->json([
@@ -85,7 +114,21 @@ class FileController extends Controller
         $process->run();
 
         if (!$process->isSuccessful()) {
-            throw new ProcessFailedException($process);
+            var_dump($process->getErrorOutput());
+            //throw new ProcessFailedException($process);
+        }
+    }
+
+    private function deviceByType($type) {
+        switch ($type) {
+            case 'jpg':
+                return 'jpeg';
+            case 'png':
+                return 'png16m';
+            case 'pdf':
+                return 'pdfwrite';
+            default:
+                return '';
         }
     }
 }
