ec30e1313a1da51ca2c35590d18b597a0c7d8a61
Natalya Shylkina <ns@albedo-tech.com>
2019-02-21 13:38:36 +0200

    add archive and convert methods

--- src/app/Http/Controllers/Api/FileController.php
+++ src/app/Http/Controllers/Api/FileController.php
@@ -6,8 +6,14 @@ use App\Helpers\ActionHelper;
 use App\Helpers\Converter;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\File;
 use Imagick;
 use Intervention\Image\Facades\Image;
+use Mockery\Exception;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 
 class FileController extends Controller
 {
@@ -22,20 +28,64 @@ class FileController extends Controller
     }
     public function convert(Request $request, $type)
     {
-//        $converter = new Converter($type);
         $types = explode('-to-', $type);
 
-        foreach ($request->all() as $key => $value) {
-//            $converter->setFile($request->file($key));
-//            $converter->convert();
-            try {
-                $imagick = new Imagick($request->file($key));
-                $imagick->setImageFormat($types[1]);
-                var_dump('test');
-            } catch (\ImagickException $e) {
+        $uid = uniqid();
+        $destination = storage_path("images/$uid");
+
+        File::makeDirectory($destination);
+
+        try {
+            foreach ($request->files as $name => $file) {
+                $from = $file->getRealPath();
+                $to = "$destination/$name.$types[1]";
+
+                $this->runProcess("convert $from $to");
             }
+            return response()->json([
+                'ok' => $uid,
+                'errors' => false
+            ], 200);
+        }
+        catch (\Exception $e) {
+            return response()->json([
+                'ok' => false,
+                'errors' => $e->getMessage()
+            ], 500);
+        }
+    }
+    public function archive($uid) {
+        $folder = storage_path("images/$uid/");
+
+        if(!File::exists($folder)) {
+            return response()->json([
+                'ok' => false,
+                'errors' => 'Destination folder not exists'
+            ], 404);
+        }
+
+        try {
+            $zipPath = "$folder$uid.zip";
+            $this->runProcess("cd $folder; zip -c $zipPath *");
+
+            return response()
+                ->download($zipPath)
+                ->deleteFileAfterSend(false);
+        }
+        catch (\Exception $e) {
+            return response()->json([
+                'ok' => false,
+                'errors' => $e->getMessage()
+            ], 500);
         }
     }
 
+    private function runProcess($command) {
+        $process = new Process($command);
+        $process->run();
 
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+    }
 }
