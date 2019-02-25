116a119c9c463bb076670c11bd7552c11ce0cfdf
Natalya Shylkina <ns@albedo-tech.com>
2019-02-25 11:31:06 +0200

    change handler instance creation

--- src/app/Http/Controllers/Api/FileController.php
+++ src/app/Http/Controllers/Api/FileController.php
@@ -9,19 +9,20 @@ class FileController extends Controller
 {
     public function compress(Request $request, $type)
     {
-        return $this->runHandler($request->files, $type, 'App\Helpers\Handler\Tools\Compressor');
+        return $this->runHandler($request->files, $type, 'Compressor');
     }
 
     public function convert(Request $request, $types)
     {
-        return $this->runHandler($request->files, $types, 'App\Helpers\Handler\Tools\Converter');
+        return $this->runHandler($request->files, $types, 'Converter');
     }
 
     private function runHandler($files, $types, $FileHandler)
     {
         try
         {
-            $fileHandler = new $FileHandler($files, $types);
+            $instance = "App\Helpers\Handler\Tools\\$FileHandler";
+            $fileHandler = new $instance($files, $types);
         }
         catch (\Exception $e)
         {
