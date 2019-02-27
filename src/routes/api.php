1ae52681e34039efb3a4f5681b5521d9580b25cc
Natalya Shylkina <ns@albedo-tech.com>
2019-02-27 12:04:58 +0200

    add auth middleware to file handler routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,21 +1,13 @@
 <?php
 
-/*
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-*/
-Route::group([
-    'middleware' => 'api',
-    'prefix' => 'auth'
-], function ($router) {
+Route::prefix('auth')->group(function () {
     Route::post('sign-in', 'AuthController@login');
     Route::post('sign-up', 'AuthController@register');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
 });
 
-Route::namespace('Api')->group(function () {
+Route::middleware('auth:api')->namespace('Api')->group(function () {
     Route::post('compressors/{type}', 'FileController@compress');
     Route::post('converters/{type}', 'FileController@convert');
     Route::get('archive/{uid}', 'FileController@archive');
